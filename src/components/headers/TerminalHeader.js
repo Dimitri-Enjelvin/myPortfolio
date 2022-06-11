760951eec5ad7e4b144849764d7180e9a00f7c68
Dimitri Enjelvin <dimitri.enjelvin@gmail.com>
2022-06-11 17:00:50 +0200

    preparing files

--- src/components/headers/TerminalHeader.js
+++ src/components/headers/TerminalHeader.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import './TerminalHeader.scss'
+
+import BubbleButton from '../buttons/BubbleButton'
+import TerminalLink from '../links/TerminalLink'
 
 
 const TerminalHeader = () => {
   return (
-    <div className="header_container">
+    <div className="header-container">
 
+      <div className="bubbles-container">
+        <BubbleButton color="red" />
+        <BubbleButton color="orange" />
+        <BubbleButton color="green" />
+      </div>
+
+      <div className="link-container">
+        <TerminalLink />
+      </div>
 
     </div>
   )
---
+++ src/components/windows/TerminalWindow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TerminalWindow.scss';
+
+const TerminalWindow = () => {
+
+    return (
+        <div className="terminal-window">
+
+        </div>
+    )
+}
+
+export default TerminalWindow
