a56a9df8cd918a15a8f9eeae81d876b51d772f77
Dimitri Enjelvin <dimitri.enjelvin@gmail.com>
2022-06-11 14:54:37 +0200

    add packages + homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,13 @@ module.exports = {
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      "icon": "src/assets/images/icon.png"
     }
   }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
-      "path": "./src/images/"
+      "path": "./src/assets/images/"
     },
     __key: "images"
   }]
